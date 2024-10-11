c368c76ab5b385168fff54f124a6d9c062ea8a41
Sejuti Mannan <sejutinm@gmail.com>
2024-10-11 13:42:08 -0400

    Wrote test for title

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -15,8 +15,17 @@ import server.endpoints as ep
 
 TEST_CLIENT = ep.app.test_client()
 
-
 def test_hello():
     resp = TEST_CLIENT.get(ep.HELLO_EP)
     resp_json = resp.get_json()
     assert ep.HELLO_RESP in resp_json
+
+
+def test_title():
+    resp = TEST_CLIENT.get(ep.TITLE_EP)
+    resp_json = resp.get_json()
+    assert ep.TITLE_RESP in resp_json
+    assert isinstance(resp_json[ep.TITLE_RESP], str)
+    assert len(resp_json[ep.TITLE_RESP]) > 0
+    # print(f'{ep.TITLE_EP}')
+    # print(f'{resp_json=}')
