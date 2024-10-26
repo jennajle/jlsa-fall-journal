e969de12c1eb3d2c270287743a92b50d9d6d816e
leosong3108 <zs2364@nyu.edu>
2024-10-26 12:16:32 -0400

    Revert "added test_roles"
    This reverts commit 5bc01a52422dc9f23dec4a88742770b059924bbe.

--- data/tests/test_roles.py
+++ data/tests/test_roles.py
@@ -5,16 +5,5 @@ def test_get_roles():
     assert isinstance(roles, dict)
     assert len(roles) > 0
     for code, role in roles.items():
-        
         assert isinstance(code,str)
         assert isinstance(role,str)
-      
-        assert 'name' in role
-        assert 'description' in role
-        assert 'can_edit' in role
-        assert 'can_review' in role
-        
-        assert isinstance(role['name'], str)
-        assert isinstance(role['description'], str)
-        assert isinstance(role['can_edit'], bool)
-        assert isinstance(role['can_review'], bool)
