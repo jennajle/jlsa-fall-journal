90114309f55e4cb71cfc6bcf78b909c36d22805f
leosong3108 <zs2364@nyu.edu>
2024-10-26 11:46:00 -0400

    Fix W293 whitespace issue in roles.py

--- data/roles.py
+++ data/roles.py
@@ -1,21 +1,23 @@
-
 """
 This module manages person roles for a journal.
 """
 from copy import deepcopy
+from data import roles as rls
 
-AUTHOR_CODE = 'AU'
+
+AUTHOR_CODE = "AU"
 TEST_CODE = AUTHOR_CODE
-ED_CODE = 'ED'
-ME_CODE = 'ME'
-CE_CODE = 'CE'
+ED_CODE = "ED"
+ME_CODE = "ME"
+CE_CODE = "CE"
+RE_CODE = "RE"
 
 ROLES = {
-    AUTHOR_CODE: 'Author',
-    CE_CODE: 'Consulting Editor',
-    ED_CODE: 'Editor',
-    ME_CODE: 'Managing Editor',
-    'RE': 'Referee',
+    AUTHOR_CODE: "Author",
+    CE_CODE: "Consulting Editor",
+    ED_CODE: "Editor",
+    ME_CODE: "Managing Editor",
+    RE_CODE: "Referee",
 }
 
 MH_ROLES = [CE_CODE, ED_CODE, ME_CODE]
@@ -43,11 +45,11 @@ def get_role_codes() -> list:
 def is_valid(code: str) -> bool:
     return code in ROLES
 
-    
+
 def main():
     print(get_roles())
     print(get_masthead_roles())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
