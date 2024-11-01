5d71f87148719f4271c97a83ec2140f06ef42108
leosong3108 <zs2364@nyu.edu>
2024-11-01 00:16:57 -0400

    revert test_people.py

---
+++ data/tests/test_people.py
@@ -0,0 +1,32 @@
+import pytest
+import data.people as ppl
+
+from data.roles import TEST_CODE as TEST_ROLE_CODE
+
+NO_AT = 'jkajsd'
+NO_NAME = '@kalsj'
+NO_DOMAIN = 'kajshd@'
+NO_SUB_DOMAIN = 'kajshd@com'
+DOMAIN_TOO_SHORT = 'kajshd@nyu.e'
+DOMAIN_TOO_LONG = 'kajshd@nyu.eedduu'
+
+TEMP_EMAIL = 'temp_person@temp.org'
+
+@pytest.fixture(scope='function')
+def temp_person():
+    _id = ppl.create_person({
+        'name': 'John Doe',
+        'roles': ['AU'],
+        'affiliation': 'NYU',
+        'email': TEMP_EMAIL
+    })
+    yield _id
+    ppl.delete_person(_id)
+
+def test_has_role(temp_person):
+    person_rec = ppl.read_one(temp_person)
+    assert ppl.has_role(person_rec, TEST_ROLE_CODE)
+
+def test_doesnt_have_role(temp_person):
+    person_rec = ppl.read_one(temp_person)
+    assert not ppl.has_role(person_rec, 'Not a good role!')
