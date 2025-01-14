a935190cbd99c1bd0aa779e7248ab47cb90074e8
leosong3108 <zs2364@nyu.edu>
2025-01-13 22:32:23 -0500

    fixing errors

--- tempCodeRunnerFile.sh
+++ tempCodeRunnerFile.sh
@@ -23,21 +23,10 @@ then
     exit 1
 fi
 
-if [ -z "$API_TOKEN" ]
-then
-    echo "The API_TOKEN var must be set in the env."
-    exit 1
-fi
-
 echo "PA user = $PA_USER"
 echo "PA password = $DEMO_PA_PWD"
 
 echo "SSHing to PythonAnywhere."
 sshpass -p $DEMO_PA_PWD ssh -o "StrictHostKeyChecking no" $PA_USER@ssh.pythonanywhere.com << EOF
-    # Export the API_TOKEN so it's available in PythonAnywhere
-    export API_TOKEN="$API_TOKEN"
-    # Navigate to project directory
-    cd ~/$PROJ_DIR
-    # Run rebuild script
-    PA_USER=$PA_USER PROJ_DIR=~/$PROJ_DIR VENV=$VENV PA_DOMAIN=$PA_DOMAIN ./rebuild.sh
+    cd ~/$PROJ_DIR; PA_USER=$PA_USER PROJ_DIR=~/$PROJ_DIR VENV=$VENV PA_DOMAIN=$PA_DOMAIN ./rebuild.sh
 EOF
