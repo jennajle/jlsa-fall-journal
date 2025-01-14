c5051ac11f3edc05b176bda66605b186e7a72a25
Sejuti Mannan <sejutinm@gmail.com>
2025-01-13 21:35:05 -0500

    fixed virtual env file activation

--- rebuild.sh
+++ rebuild.sh
@@ -9,7 +9,7 @@ echo "Pulling code from master"
 git pull origin master
 
 echo "Activate the virtual env $VENV for user $PA_USER"
-source /home/$PA_USER/.virtualenvs/$VENV/bin/activate
+source /home/$PA_USER/jlsa-fall-journal/$VENV/bin/activate
 
 echo "Install packages"
 pip install --upgrade -r requirements.txt
