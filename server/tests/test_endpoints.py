853ad775c36de23eb3caa0afc96f9d85afadbe36
leosong3108 <zs2364@nyu.edu>
2025-02-13 10:40:24 -0500

    Revert "Update test_endpoints.py"
    This reverts commit ff06fd42bc89ddd7c96077f3dc3967ff419ef9c8.

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -88,16 +88,6 @@ def test_add_role(mock_add_role, mock_read_one):
     'affiliation': 'NYU',
     'email': 'johndoe@nyu.edu'
 })
-
-
-@patch(PEOPLE_LOC + 'update', autospec=True, return_value={
-    'name': 'Jane Doe',
-    'roles': ['AU', 'Editor'],
-    'affiliation': 'NYU',
-    'email': 'janedoe@nyu.edu'
-})
-
-
 @patch('data.people.remove_role', autospec=True)
 def test_delete_role(mock_remove_role, mock_read_one):
     role = "AU"
@@ -186,22 +176,3 @@ def test_handle_action(mock_read):
                                manu.ACTION: 'some action',
                            })
     assert resp.status_code == OK
-
-
-def test_update_person(mock_update):
-    update_data = {
-        'name': 'Jane Doe',
-        'roles': ['AU', 'Editor'],
-        'affiliation': 'NYU',
-        'email': 'janedoe@nyu.edu'
-    }
-
-    resp = TEST_CLIENT.put(ep.PEOPLE_EP, json=update_data)
-    resp_json = resp.get_json()
-
-    assert resp.status_code == CREATED
-    assert 'Person updated successfully' in resp_json['Message']
-
-    mock_update.assert_called_once_with(
-        'Jane Doe', 'NYU', 'janedoe@nyu.edu', ['AU', 'Editor']
-    )
